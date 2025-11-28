c97e49f79cbfbd34dab4d6be6a49c9e3d20a3655
Dhananjay Chouhan <dhananjaymewada62@gmail.com>
2025-11-29 01:09:39 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,38 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+// int arr[4]={1,2,3,4};
+// for (int i=0;i<4;i++){
+//     cout<<arr[i]<<" ";
+// }
+// return 0;
+// }
+ 
+
+//user input
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[5];
+   
+//     for (int i=0;i<5;i++){
+      
+//         cin>>arr[i];
+
+//     }
+
+// return 0;
+// }
+
+//char data type
+
+#include<iostream>
+using namespace std;
+int main(){
+char arr[5]={'s','d','s','d','d'};
+for (char i =0;i<5;i++){
+    cout<<arr[i]<<" ";
+}
+return 0;
+}
