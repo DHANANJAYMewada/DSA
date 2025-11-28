b02f1976f59e52ac22054ec613796d726ecf3b54
Dhananjay Chouhan <dhananjaymewada62@gmail.com>
2025-11-29 01:22:11 +0530

    Add files via upload

---
+++ roted array.cpp
@@ -0,0 +1,13 @@
+// roted array
+#include<iostream>
+using namespace std;
+int main(){
+int arr[4]={1,2,3,4};
+int a=arr[0];
+int b=arr[4];
+
+for (int i=0;i<4;i++){
+    cout<<arr[i]<<" ";
+}
+return 0;
+}
