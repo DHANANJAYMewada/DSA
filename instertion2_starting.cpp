67c7f6fbf87d69345dca205f642d27a32d039b62
Dhananjay Chouhan <dhananjaymewada62@gmail.com>
2025-12-11 11:38:48 +0530

    Add files via upload

---
+++ instertion2_starting.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node*next;
+    node*prev;
+    node(int value){
+        data=value;
+        next=NULL;
+        prev=NULL;
+
+    }
+};
+
+int main(){
+    node*head;
+    head=NULL;
+    node*tail=head;
+    int arr[5]={1,2,3,4,5};
+    for(int i=0; i<5;i++){
+        if(head==NULL){
+            head=new node(arr[i]);
+            tail=head;
+
+        }
+        else{
+            node*temp;
+            temp=new node(arr[i]);
+            tail->next=temp;
+            temp->prev=tail;
+            tail=temp;
+        }
+        }
+        node*tem=head;
+        while(tem){
+            cout<<tem->data<<" ";
+            tem=tem->next;
+        
+    }
+
+return 0;
+}
